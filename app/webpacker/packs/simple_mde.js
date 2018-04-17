cf4e1594823799385dcef6cfadc5b86d2e20438c
Philippe Virouleau <philippe.44@gmail.com>
2018-04-17 16:55:53 +0200

    adjust how simple mde is declared

--- app/webpacker/packs/simple_mde.js
+++ app/webpacker/packs/simple_mde.js
@@ -1,1 +1,7 @@
-require("expose-loader?SimpleMDE!simplemde")
+import SimpleMDE from "simplemde";
+$(".enable-simplemde").each(function() {
+  // By default SimpleMDE loads itself on the first textarea, which is what we want.
+  new SimpleMDE({ element: this, spellChecker: false });
+  // Prevent the turbolink reload from enabling twice simplemde on the same element.
+  $(this).removeClass("enable-simplemde");
+});
