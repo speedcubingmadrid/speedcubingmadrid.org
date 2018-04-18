b9b6ee71458c22a67852678d9cab2aba2d740dd2
Philippe Virouleau <philippe.44@gmail.com>
2018-04-18 15:14:30 +0200

    Compute slug on the fly

--- app/webpacker/afs/index.js
+++ app/webpacker/afs/index.js
@@ -2,3 +2,13 @@
 $(document).on('turbolinks:load', function() {
   $('[data-toggle="tooltip"]').tooltip();
 })
+
+window.afs = window.afs || {};
+
+window.afs.computeSlug = function(title) {
+  let normalized = title.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+  normalized = normalized.replace(/['"]/g, "");
+  normalized = normalized.replace(/ /g, "-");
+  normalized = normalized.toLowerCase();
+  return normalized;
+}
