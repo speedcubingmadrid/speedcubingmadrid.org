61488db5d679f024f40455834ffc58c61074b23f
Philippe Virouleau <philippe.44@gmail.com>
2018-06-11 19:38:49 +0200

    upgrade upath for node v10

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 1 }
 threads threads_count, threads_count
 
