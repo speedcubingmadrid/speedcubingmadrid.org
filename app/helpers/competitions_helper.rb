a79e18b39af4eb7f38f3635072f260d6f2bd1230
AlbertoPdRF <albertopdrf.wca@gmail.com>
2018-11-24 23:31:25 +0100

    Some minor tweaks.

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -2,9 +2,9 @@ module CompetitionsHelper
   def class_and_status_from_person(sub_by_id, sub_by_name, person)
     if @subscribers_by_id.include?(person["wcaId"]) ||
        @subscribers_by_name.include?(person["name"].downcase)
-      ["success", "Organizado por la AMS"]
+      ["success", "Socio de la AMS"]
     elsif person["wcaId"]
-      ["danger", "Doit payer"]
+      ["danger", "Sin descuento"]
     else
       ["warning", link_to("Comprueba si es un nuevo competidor", "#", class: "link-check", data: { id: person["registration"]["wcaRegistrationId"] })]
     end
